93bcb64a47eacb5287acdee23a60a983a729aa25
zhuyn <zhuyanan@powerbeijing.com>
2018-11-23 17:15:09 +0800

    Model

---
+++ ichat/Class/Main/Other/ICHModel.swift
@@ -0,0 +1,18 @@
+//
+//  ICHModel.swift
+//  ichat
+//
+//  Created by Inbasis on 2018/11/23.
+//  Copyright © 2018年 iChat. All rights reserved.
+//
+
+import Foundation
+import HandyJSON
+
+// 启停机组模型
+struct ContactModel: HandyJSON {
+    var userName: String?
+    var friendId: String?
+    var userId: String?
+    var createDate: String?
+}
